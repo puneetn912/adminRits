c9a4582e9c9eeffaa736886cf3e04676b65f00d8
Puneet Naik <naikpuneetgs@gmail.com>
2019-08-03 19:12:57 +0530

    s

---
+++ build/precache-manifest.6a3efc8328db25aa403cebc4ba1dafcc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bea97d8019987bde02a2c98aa6b9aa3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d1dbe6f212571ff4d86a",
+    "url": "/static/css/2.fa4a79a6.chunk.css"
+  },
+  {
+    "revision": "4eb293ba64d352f8c8ad",
+    "url": "/static/css/main.fe0766aa.chunk.css"
+  },
+  {
+    "revision": "d1dbe6f212571ff4d86a",
+    "url": "/static/js/2.01100694.chunk.js"
+  },
+  {
+    "revision": "4eb293ba64d352f8c8ad",
+    "url": "/static/js/main.1a0d84a9.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
